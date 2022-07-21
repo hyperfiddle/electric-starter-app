a2e114cfd9aba8b2b69ae2eb096a29a5c01d0458
Geoffrey Gaillard <ggaillard.cvl@gmail.com>
2022-07-21 11:10:31 +0200

    Add auto `Content-Type` handling

--- src/server.clj
+++ src/server.clj
@@ -1,11 +1,19 @@
 (ns server
   (:require [ring.middleware.file :refer [wrap-file]]
+            [ring.middleware.content-type :refer [wrap-content-type]]
             [ring.adapter.jetty9 :as ring]
             [hyperfiddle.photon-jetty-adapter :as adapter]
             [hyperfiddle.logger :as log])
   (:import [java.io IOException]
            [java.net BindException]
            ))
+
+(defn wrap-default-page [next-handler]
+  (fn [ring-req]
+    (case (:uri ring-req)
+      ("" "/") (next-handler (assoc ring-req :uri "/index.html"))
+      (next-handler ring-req))))
+
 #_
 (defn wrap-photon [next-handler]        ; Jetty 10 allows such handler
   (fn [ring-request]
@@ -21,6 +29,8 @@
   (try
     (ring/run-jetty (-> default-handler
                       (wrap-file "resources/public")
+                      (wrap-content-type)
+                      (wrap-default-page)
                       #_(wrap-photon))
       ;; Jetty 9 forces us to declare WS paths out of a ring handler.
       (merge {:port       8080
