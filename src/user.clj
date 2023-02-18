9cba8f58a2c3d97c2aece06f2641dacf0b7f466d
Dustin Getz <dustin.getz@gmail.com>
2023-02-18 15:44:31 -0500

    refactor user.clj entrypoint to align with deps.edn :dev alias (no shadow without dev alias)

--- src/user.clj
+++ src/user.clj
@@ -1,10 +1,22 @@
-(ns user ; Must be ".clj" file, Clojure doesn't auto-load user.cljc
-  (:require hyperfiddle.electric-jetty-server
-            shadow.cljs.devtools.api
-            shadow.cljs.devtools.server))
+(ns user) ; Must be ".clj" file, Clojure doesn't auto-load user.cljc
 
-; Userland Electric code is lazy loaded by the shadow build due to usage of :require-macros
-; in all Electric source files.
+; lazy load dev stuff - for faster REPL startup and cleaner dev classpath
+(def start-electric-server! (delay @(requiring-resolve 'hyperfiddle.electric-jetty-server/start-server!)))
+(def shadow-start! (delay @(requiring-resolve 'shadow.cljs.devtools.server/start!)))
+(def shadow-watch (delay @(requiring-resolve 'shadow.cljs.devtools.api/watch)))
+
+(def electric-server-config
+  {:host "0.0.0.0", :port 8080, :resources-path "resources/public"})
+
+(defn main [& args]
+  (println "Starting Electric compiler and server...")
+  (@shadow-start!) ; serves index.html as well
+  (@shadow-watch :dev) ; depends on shadow server
+  (def server (@start-electric-server! electric-server-config))
+  (comment (.stop server)))
+
+; Userland Electric code is lazy loaded by the shadow build due to usage of
+; :require-macros in all Electric source files.
 ; WARNING: make sure your REPL and shadow-cljs are sharing the same JVM!
 
 (comment
@@ -14,16 +26,3 @@
   ; connect a second REPL instance to it
   ; (DO NOT REUSE JVM REPL it will fail weirdly)
   (type 1))
-
-(def electric-server-config
-  {:host "0.0.0.0", :port 8080, :resources-path "resources/public"})
-
-(defn main [& args]
-  (println "Starting Electric compiler and server...")
-  (shadow.cljs.devtools.server/start!) ; serves index.html as well
-  (shadow.cljs.devtools.api/watch :dev) ; depends on shadow server
-  (def server (hyperfiddle.electric-jetty-server/start-server! electric-server-config))
-  (comment (.stop server)))
-
-(when (= "true" (get (System/getenv) "HYPERFIDDLE_AUTO_BOOT"))
-  (main))
