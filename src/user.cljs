7d6f183e646faccbe94219e22915d1da366dae1f
Dustin Getz <dustin.getz@gmail.com>
2023-04-11 16:05:15 -0400

    entrypoint - tighten, begin content

--- src/user.cljs
+++ src/user.cljs
@@ -1,4 +1,4 @@
-(ns ^:dev/always user ; Electric currently needs to rebuild everything when any file changes. Will fix
+(ns ^:dev/always user ; rebuild everything when any file changes. Will fix
   (:require hyperfiddle.electric
             hyperfiddle.rcf
             user-main))
@@ -11,8 +11,9 @@
                  #(js/console.log "Reactor success:" %)
                  (fn [error]
                    (case (:hyperfiddle.electric/type (ex-data error))
-                     :hyperfiddle.electric-client/stale-client (do (js/console.log "Server and client version mismatch. Refreshing page.")
-                                                                   (.reload (.-location js/window)))
+                     :hyperfiddle.electric-client/stale-client 
+                     (do (js/console.log "Client/server version mismatch, refreshing page.")
+                       (.reload (.-location js/window))) 
                      (js/console.error "Reactor failure:" error)))))
   (hyperfiddle.rcf/enable!))
 
---
+++ src/user.md
@@ -0,0 +1,14 @@
+hello world
+
+```clojure
+(ns prod
+  (:require hyperfiddle.electric
+            user-main))
+
+(def electric-main (hyperfiddle.electric/boot (user-main/Main.)))
+
+(defn start! []
+  (electric-main
+    #(js/console.log "Reactor success:" %)
+    #(js/console.error "Reactor failure:" %)))
+```
