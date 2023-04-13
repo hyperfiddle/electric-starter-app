97973cd08ab6aaba4ce750f99a5002cf829626ec
Dustin Getz <dustin.getz@gmail.com>
2023-04-13 10:24:46 -0400

    upgrade electric to v2-283 and fix refresh-on-stale-reconnect

--- src/prod.clj
+++ src/prod.clj
@@ -1,7 +1,11 @@
 (ns prod
   (:gen-class)
   (:require app.todo-list ; in prod, load app into server so it can accept clients
+            clojure.string
             electric-server-java8-jetty9))
+
+(when (clojure.string/blank? (System/getProperty "HYPERFIDDLE_ELECTRIC_SERVER_VERSION"))
+  (throw (ex-info "$HYPERFIDDLE_ELECTRIC_SERVER_VERSION must be set in prod" {})))
 
 (def electric-server-config
   {:host "0.0.0.0", :port 8080, :resources-path "public"})
